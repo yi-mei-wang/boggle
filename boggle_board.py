2606572664d4d4b574868ae018b5828cbd780fe0
mei <yimei.wym@gmail.com>
2019-10-25 16:46:49 +0800

    check the first letter of user's input

--- boggle_board.py
+++ boggle_board.py
@@ -2,15 +2,58 @@ import random
 import boggle_helpers as bh
 
 
-def play_game():
+def check_first_word(word, board):
+    """
+    Searches for the first letter of a given word in a board.
+
+    Parameters:
+    -----------
+    word : str
+        The string to search for
+
+    board : list
+        A 2D 4x4 list where each element is a random letter
+
+    Returns:
+    --------
+    bool
+        True if the first letter of word is found, and vice versa.
+    """
+
+    # Iterate through the rows of the board
+    for row in board:
+        # Iterate through the elements of each row
+        for elem in row:
+            if word[0] == elem:
+                return True
+    # Check if the first letter is found
+
+
+def setup():
     # Initialise an empty board
     board = bh.empty_board(4)
 
     # Shuffle the 16 dice
     dice = bh.shuffle(bh.DICE)
 
-    # Fill and print the current board
-    bh.print_board(bh.shake(dice, board))
+    # Fill the current board
+    return bh.shake(dice, board)
+
+
+def play_game():
+    board = setup()
+
+    while True:
+        bh.print_board(board)
+
+        word_to_check = input('Enter the word to be checked: ')
+
+        # Only check the rest of the word if the first word is found in the board
+        if not check_first_word(word_to_check, board):
+            print('WORD NOT FOUND')
+
+        else:
+            continue
 
 
 play_game()
