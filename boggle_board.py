3f935bd1852c1b8ed64fa2ecb362253a9c4a4a59
mei <yimei.wym@gmail.com>
2019-10-28 11:11:55 +0800

    take into account past searches

--- boggle_board.py
+++ boggle_board.py
@@ -30,10 +30,10 @@ def check_first_letter(word, board):
     return anchors
 
 
-def find_in_coords(char, x, y, board):
+def find_in_coords(char, x, y, board, history):
     """Finds a char based on the coordinates provided.
 
-    Searches for char in all the neighbouring cells of the starting point, which is represented by the x- and y-coordinates provided.
+    Searches for char in all the neighbouring cells of the starting point, which is represented by the x- and y-coordinates provided. Takes into account previous searches so a previously-used cell cannot be reused.
 
     Args:
     -----
@@ -41,28 +41,35 @@ def find_in_coords(char, x, y, board):
         x : An int representing the x-coordinate of the starting point
         y : An int representing the y-coordinate of the starting point
         board : A 2-D list whose each element is a random letter
+        history : A list of coordinates of past searches
+
+    Returns:
+    --------
+        A list of the new coordinates to search.
     """
     pos_to_check = bh.get_coords(x, y)
 
     new_coords = []
 
     for (new_x, new_y) in pos_to_check:
-        if char == board[new_y][new_x]:
-            new_coords.append((new_x, new_y))
+        if (new_x, new_y) not in history:
+            if char == board[new_y][new_x]:
+                new_coords.append((new_x, new_y))
 
     return new_coords
 
 
-def find_word(word, anchors, board):
+def find_word(word, anchors, board, history):
     """ Finds a word in a Boggle board.
 
-    Searches for a word or part thereof in a Boggle board using the coordinates given. 
+    Searches for a word or part thereof in a Boggle board using the coordinates given. Uses the backtracking algorithm to search depth first. 
 
     Args:
     -----
         word : A string representing the word to be searched for or part thereof
         anchors : A list of coordinates of where the word should be searched in relation to
         board : A 2-D list representing a Boggle board
+        history : A list of coordinates of past searches
 
     Returns:
     --------
@@ -74,15 +81,19 @@ def find_word(word, anchors, board):
         return True
 
     # Go through every occurrence of the first char of word and search for the remainder of the word
-    print('Searching in ', anchors)
     for (x, y) in anchors:
-        possibilities = find_in_coords(word[0], x, y, board)
-        print(f'{word[0]} is found in {possibilities}')
+        print(f'Searching for {word[0]} from around ({x}, {y})')
+
+        possibilities = find_in_coords(word[0], x, y, board, history)
+
+        print(f'{word[0]} is found in {possibilities}\n')
 
         # If E is found in the neighbouring cells of B,
         if possibilities:
+            history.append((x, y))
+
             # Look for ST in all the occurrences of E
-            if find_word(word[1:], possibilities, board):
+            if find_word(word[1:], possibilities, board, history):
                 return True
 
     # If all the anchors have been searched and the word is still not found, trigger backtracking
@@ -124,7 +135,7 @@ def play_game():
 
         anchors = check_first_letter(word_to_check, board)
 
-        print(find_word(word_to_check[1:], anchors, board))
+        print(find_word(word_to_check[1:], anchors, board, []))
 
 
 play_game()
