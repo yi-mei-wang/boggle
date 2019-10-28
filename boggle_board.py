b32f8af4f8dbcd45f817e3af3cbd46e661acd652
mei <yimei.wym@gmail.com>
2019-10-28 12:18:27 +0800

    load dictionary and print game status

--- boggle_board.py
+++ boggle_board.py
@@ -130,6 +130,9 @@ def setup(n):
 def play_game():
     board = setup(4)
 
+    filename = '/Users/mei/work/teaching/python-sep-19/day-3/recursion/boggle/dictionary.txt'
+    dictionary = [line.rstrip('\n') for line in open(filename)]
+
     while True:
         bh.print_board(board)
 
@@ -137,7 +140,17 @@ def play_game():
 
         anchors = check_first_letter(word_to_check, board)
 
-        print(find_word(word_to_check[1:], anchors, board, []))
+        if find_word(word_to_check[1:], anchors, board, []):
+            print('Word is found in board!\n')
+            print('Checking if word is found in dictionary...')
+
+            if bh.binary_search(word_to_check.lower(), dictionary):
+                print(f'{word_to_check} is a valid word!')
+            else:
+                print(f'{word_to_check} is not a valid word!')
+
+        else:
+            print('Word not found in board.\nPlease try with another word.')
 
 
 play_game()
