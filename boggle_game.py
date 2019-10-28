33252d3f3ebc5b38d8df1847276de47e6bb420ac
mei <yimei.wym@gmail.com>
2019-10-28 13:26:25 +0800

    rename to word_to_find

--- boggle_game.py
+++ boggle_game.py
@@ -16,19 +16,19 @@ def main():
     while True:
         bh.print_board(board)
 
-        word_to_check = input(
+        word_to_find = input(
             'Enter the word to be checked: ').upper().replace('QU', 'Q')
 
-        anchors = bs.check_first_letter(word_to_check, board)
+        anchors = bs.check_first_letter(word_to_find, board)
 
-        if bs.find_word(word_to_check[1:], anchors, board, []):
+        if bs.find_word(word_to_find[1:], anchors, board, []):
             print('Word is found in board!\n')
             print('Checking if word is found in dictionary...')
 
-            if bd.binary_search(word_to_check.lower().replace('q', 'qu'), dictionary):
-                print(f'{word_to_check} is a valid word!')
+            if bd.binary_search(word_to_find.lower().replace('q', 'qu'), dictionary):
+                print(f'{word_to_find} is a valid word!')
             else:
-                print(f'{word_to_check} is not a valid word!')
+                print(f'{word_to_find} is not a valid word!')
 
         else:
             print('Word not found in board.\n\nPlease try with another word.\n')
